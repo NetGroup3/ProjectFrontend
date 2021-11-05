ab6c3b6bcdf2056c0c25bd73fcbefcc630aaf43b
MaksimNemoshenkov <nemoshenkov.maxim@gmail.com>
2021-11-06 01:32:53 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NetProjectFrontend';
+  userData:any={};
+  getData(data:NgForm){
+    console.warn(data)
+    this.userData=data
+  }
 }
