1fc8839f8d9538b8ddee547f1b6711cad14dc7a1
Greg <realgregorygrytsyniak@gmail.com>
2021-11-13 18:11:28 +0200

    deleted unnecessary login and sign up components from app folder(the necessary one are in auth folder)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
 import { SignupPageComponent } from './modules/auth/pages/signup-page/signup-page.component';
 import { LoginPageComponent } from './modules/auth/pages/login-page/login-page.component';
+import { RecoveryPageComponent } from './modules/auth/pages/recovery-page/recovery-page.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
@@ -21,7 +20,6 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import {NzFormControlComponent, NzFormModule} from "ng-zorro-antd/form";
-import { RecoveryPageComponent } from './modules/auth/pages/recovery-page/recovery-page.component';
 import {RestapiService} from "./modules/auth/services/rest/restapi.service";
 import { HomeComponent } from './home/home.component';
 
@@ -32,8 +30,6 @@ registerLocaleData(en);
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignupComponent,
     SignupPageComponent,
     LoginPageComponent,
     RecoveryPageComponent,
