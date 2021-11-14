21bb32f83a14835df825e933d7f990499b6e73d3
Greg <realgregorygrytsyniak@gmail.com>
2021-11-14 14:22:58 +0200

    sceleton

---
+++ src/app/sceletons/user-sceleton/user-sceleton.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+
+
+@Component({
+  selector: 'app-user-sceleton',
+  templateUrl: './user-sceleton.component.html',
+  styleUrls: ['./user-sceleton.component.scss']
+})
+export class UserSceletonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+
+
+
