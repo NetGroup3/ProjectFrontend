331391fdeffbdf9f48b1444aeee731f28a2b665e
MaksimNemoshenkov <nemoshenkov.maxim@gmail.com>
2021-11-14 10:07:23 +0200

    make it better

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/ProjectFrontend'));
-
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/ProjectFrontend/'}),
-);
-
-// Start the app by listening on the default Heroku port
+app.use(express.static(__dirname + '/dist/net-project-frontend'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/net-project-frontend/index.html'));});
 app.listen(process.env.PORT || 8080);
