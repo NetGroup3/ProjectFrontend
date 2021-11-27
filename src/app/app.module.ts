badb823c673dc24ba007bb15bf89ee26239571c2
MaksimNemoshenkov <nemoshenkov.maxim@gmail.com>
2021-11-27 16:07:57 +0200

    fast fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -52,7 +52,6 @@ import {UploadImageComponent} from "./auth-user/auth-user-settings/upload-image/
 import {PersonalInformationComponent} from "./auth-user/auth-user-settings/personal-information/personal-information.component";
 import {ChangePasswordComponent} from "./auth-user/auth-user-settings/change-password/change-password.component";
 import {PicturePipe} from "./picture.pipe";
-import {UploadService} from "./modules/auth/services/client/upload.service";
 import { DishComponent } from './dish/dish.component';
 import { AddEditDishComponent } from './add-edit-dish/add-edit-dish.component';
 
