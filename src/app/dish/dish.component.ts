512e86cf392de2becc193c3b18bd0925a29156b7
gingerose <kurzhumovam41@gmail.com>
2021-11-29 11:15:16 +0200

    some fixes

--- src/app/dish/dish.component.ts
+++ src/app/dish/dish.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {ModeratorService} from "../services/moderator.service";
 import {Dish} from "../models/dish";
+import {Ingredient} from "../models/ingredient";
+import {Cloudinary, CloudinaryImage} from "@cloudinary/url-gen";
+import {thumbnail} from "@cloudinary/url-gen/actions/resize";
+import {byRadius} from "@cloudinary/url-gen/actions/roundCorners";
 
 @Component({
   selector: 'app-dish',
@@ -12,19 +16,37 @@ export class DishComponent implements OnInit {
   limit: number = 10;
   page: number = 0;
   Dishes: Dish[] = [];
+  key: string = ""
+  category: string = ""
+  sortedBy: string = ""
+
+  delDish: Dish = {
+    id:  0,
+    title: "",
+    image_id: "",
+    description: "",
+    category: "",
+    receipt: "",
+    active: false
+  }
 
   constructor(private moderatorService: ModeratorService) {
   }
   toggle: boolean = true;
+  id: boolean = false;
+  title: boolean = false;
+  Category: boolean = false;
+
   ngOnInit(): void {
-    this.getDish(this.limit, this.page);
+    this.search()
   }
 
-  getDish(limit: number, page: number): void {
-    this.moderatorService.get_dishes(limit, page)
+  getIngridients(limit: number, page: number, key: string, category: string, sortedBy: string): void {
+    console.log(key)
+    this.moderatorService.get_dishes(limit, page, key, category, sortedBy)
       .subscribe((response:any)=>{
-        console.log(response.body)
-        this.Dishes = response.body
+        console.log(response)
+        this.Dishes = response
       });
   }
 
@@ -37,6 +59,7 @@ export class DishComponent implements OnInit {
       this.page = this.page + 1;
       this.ngOnInit();
     }
+
   }
 
   prev() {
@@ -46,16 +69,43 @@ export class DishComponent implements OnInit {
     }
   }
 
-  cancel() {
+  change() {
     this.toggle = !this.toggle;
   }
 
-  ok(id: number) {
+  ok() {
     this.toggle = !this.toggle;
-    this.moderatorService.delete_dish(id).subscribe((response:any)=>{
+    console.log(this.delDish)
+    this.moderatorService.delete_dish(this.delDish.id).subscribe((response:any)=>{
       console.log(response)
     });
+    location.reload();
   }
 
+  initImage(imageId: string): CloudinaryImage {
+    const cld = new Cloudinary({cloud: {cloudName: 'djcak19nu'}});
+    return cld.image(imageId)
+      .resize(thumbnail().width(50).height(50))
+      .roundCorners(byRadius(10));
+  }
 
+  delete(dish: Dish) {
+    this.delDish = dish;
+    this.toggle = !this.toggle;
+
+  }
+
+  search() {
+    if(this.id){
+      this.sortedBy = "id"
+    }
+    else if(this.title){
+      console.log(111111111)
+      this.sortedBy = "title"
+    }
+    else if(this.category){
+      this.sortedBy = "category"
+    }
+    this.getIngridients(this.limit, this.page, this.key, this.category, this.sortedBy);
+  }
 }
