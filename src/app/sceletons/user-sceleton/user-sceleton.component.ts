812a1bbbcacd4b959b6115499e772581fc21cb4b
Greg <realgregorygrytsyniak@gmail.com>
2021-12-01 14:37:36 +0200

    first change

--- src/app/sceletons/user-sceleton/user-sceleton.component.ts
+++ src/app/sceletons/user-sceleton/user-sceleton.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-user-sceleton',
@@ -8,26 +9,16 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class UserSceletonComponent implements OnInit {
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
-
-  private readonly TOKEN_KEY: string = "AUTH_TOKEN";
-  private readonly USER_ID: string = "USER_ID";
-  private readonly USER_FIRSTNAME: string = "USER_FIRSTNAME";
-  private readonly USER_LASTNAME: string = "USER_LASTNAME";
-  private readonly USER_ROLE: string = "USER_ROLE";
-  private readonly USER_IMAGE_ID: string = "USER_IMAGE_ID";
-
-  ngOnInit(): void {
+  constructor(private router: Router, private route: ActivatedRoute) {
   }
 
-  logOut() {
-    localStorage.setItem(this.TOKEN_KEY, "");
-    localStorage.setItem(this.USER_ID, "");
-    localStorage.setItem(this.USER_FIRSTNAME, "");
-    localStorage.setItem(this.USER_LASTNAME, "");
-    localStorage.setItem(this.USER_ROLE, "");
-    localStorage.setItem(this.USER_IMAGE_ID, "");
-    this.router.navigate(['/login']);
+  private readonly TOKEN_KEY: string = "AUTH_TOKEN";
+
+
+  ngOnInit(): void {
+    if (localStorage.getItem(this.TOKEN_KEY) === "") {
+      this.router.navigate(['/login']);
+    }
   }
 
 }
