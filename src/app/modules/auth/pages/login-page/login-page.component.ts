10f311169d340b7863010c636d1b5a3ed00d27dc
deavery <81487538+deavery@users.noreply.github.com>
2021-12-06 16:19:51 +0200

    Mail links

--- src/app/modules/auth/pages/login-page/login-page.component.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -3,6 +3,7 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import {AuthRestService} from "../../services/rest/auth-rest.service";
 import {AuthService} from "../../services/client/auth.service";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -11,18 +12,29 @@ import {AuthService} from "../../services/client/auth.service";
 })
 export class LoginPageComponent implements OnInit {
 
+  private verify: any;
+  private recovery: any;
+
   constructor(
     private fb: FormBuilder,
     private authRestService: AuthRestService,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private activateRoute: ActivatedRoute
   ) {
   }
 
   public form: FormGroup = this.buildForm();
 
   ngOnInit(): void {
+    this.activateRoute.queryParams.subscribe(params => {
+      this.verify = params['verify'];
+      this.recovery = params['recovery'];
+      if ( this.verify != null) this.verifyCode();
+      if ( this.recovery != null) this.recoveryCode();
+    });
   }
+
 
 
   public onLoginClick(): void {
@@ -45,6 +57,18 @@ export class LoginPageComponent implements OnInit {
       username: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(128)]]
     });
+  }
+
+  private verifyCode(): void {
+    this.authRestService.code(this.verify).subscribe((response: any) => {
+      console.log(response)
+    })
+  }
+
+  private recoveryCode(): void {
+    this.authRestService.code(this.recovery).subscribe((response: any) => {
+      console.log(response)
+    })
   }
 
 }
