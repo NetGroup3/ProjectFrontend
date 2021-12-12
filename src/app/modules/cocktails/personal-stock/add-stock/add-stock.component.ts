aba099b27d3726dc3d038e4dd28759d3b246b25a
MaksimNemoshenkov <nemoshenkov.maxim@gmail.com>
2021-12-12 22:52:21 +0200

    add notifications to personal stock

--- src/app/modules/cocktails/personal-stock/add-stock/add-stock.component.ts
+++ src/app/modules/cocktails/personal-stock/add-stock/add-stock.component.ts
@@ -1,11 +1,11 @@
 import {Component, OnInit, Output, EventEmitter, Input, OnChanges, SimpleChanges} from '@angular/core';
-import {catchError} from "rxjs/operators";
-import {of} from "rxjs";
 import {StockService} from "../../../core/services/stock.service";
 import {Ingredient} from "../../../core/models/ingredient";
 import {StockAddDto} from "../../../core/models/StockAddDto";
 import {UiService} from "../../../core/services/ui.service";
 import {Subscription} from "rxjs";
+import {StoreStockService} from "../store-stock.service";
+import {NzNotificationService} from "ng-zorro-antd/notification";
 
 @Component({
   selector: 'app-add-stock',
@@ -24,13 +24,14 @@ export class AddStockComponent implements OnInit, OnChanges {
   selectedIngredientId: number = 0;
   amount: number = 0;
 
-  ingredients: Ingredient[] = [];
   showAddStock: boolean = false;
   subscription!: Subscription;
 
   constructor(
     private stockService: StockService,
     private uiService: UiService,
+    private notification: NzNotificationService,
+    public storeStockService: StoreStockService
   ) {
     this.subscription = this.uiService
       .onToggle()
@@ -38,16 +39,17 @@ export class AddStockComponent implements OnInit, OnChanges {
   }
 
   ngOnInit(): void {
-    this.getIngredients((res: any) => {
-      this.ingredients = res;
-    });
+    this.getIngredients();
   }
 
-  getIngredients(callback: (res: any) => void): void {
+  getIngredients(): void {
     this.stockService
       .getIngredients(this.limit, this.page)
-      .pipe(catchError(() => of({ results: [] })))
-      .subscribe((res: any) => callback(res));
+      .subscribe((ingredients: Ingredient[])=>{
+        this.storeStockService.ingredients = ingredients;
+    },()=>{
+        this.notification.error("Failed to load ingredients from server", "");
+        });
   }
 
   loadMore() {
@@ -59,13 +61,13 @@ export class AddStockComponent implements OnInit, OnChanges {
     this.stockService.getIngredients(this.limit, this.page)
       .subscribe(data => {
         this.isLoading = false;
-        this.ingredients = [...this.ingredients, ...data];
+        this.storeStockService.ingredients = [...this.storeStockService.ingredients, ...data];
       });
   }
 
   onSubmit(){
     if(!this.amount){
-      alert("Please add amount!");
+      this.notification.error("Please add amount!", "");
       return;
     }
     const stockAddDto: StockAddDto = {
@@ -73,14 +75,13 @@ export class AddStockComponent implements OnInit, OnChanges {
       amount: this.amount
     }
     this.onAddStock.emit(stockAddDto);
-    this.ingredients = this.ingredients.filter(ingredient=>ingredient.id!==this.selectedIngredientId);
     this.selectedIngredientId = 0;
     this.amount = 0;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if(this.ingredient){
-      this.ingredients.push(this.ingredient);
+      this.storeStockService.ingredients.push(this.ingredient);
     }
   }
 
--- src/app/modules/cocktails/personal-stock/personal-stock.component.ts
+++ src/app/modules/cocktails/personal-stock/personal-stock.component.ts
@@ -6,6 +6,7 @@ import {Ingredient} from "../../core/models/ingredient";
 import {StockAddDto} from "../../core/models/StockAddDto";
 import {UiService} from "../../core/services/ui.service";
 import {NzNotificationService} from "ng-zorro-antd/notification";
+import {StoreStockService} from "./store-stock.service";
 
 @Component({
   selector: 'app-personal-stock',
@@ -19,7 +20,7 @@ export class PersonalStockComponent implements OnInit {
   public showAddStock: boolean = false;
   public subscription!: Subscription;
 
-  private limit: number = 11;
+  private limit: number = 20;
   private page: number = 0;
   private pages: number = 0;
   private searchText: string = "";
@@ -28,14 +29,14 @@ export class PersonalStockComponent implements OnInit {
 
   public isLoading = false;
 
-  public stocks: Stock[] = [];
   public selectedIngredientId: number = 0;
-  public amount: number = 0;
+  public amount: number = 1;
 
   constructor(
     private stockService: StockService,
     private uiService: UiService,
     private notification: NzNotificationService,
+    public storeStockService: StoreStockService
   ) {
     this.subscription = this.uiService
       .onToggle()
@@ -43,7 +44,7 @@ export class PersonalStockComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getData();
+    this.getStock();
     this.getPages();
   }
 
@@ -73,7 +74,7 @@ export class PersonalStockComponent implements OnInit {
     });
   }
 
-  getData(): void {
+  getStock(): void {
     if (this.isLoading){
       return;
     }
@@ -85,7 +86,7 @@ export class PersonalStockComponent implements OnInit {
       this.category,
       this.sortedBy
     ).subscribe((stocks: Stock[]) => {
-        this.stocks = stocks;
+        this.storeStockService.stocks = stocks;
         this.isLoading = false;
         this.page++;
       });
@@ -104,7 +105,7 @@ export class PersonalStockComponent implements OnInit {
       this.sortedBy
     ).subscribe((stocks: Stock[]) => {
       this.isLoading = false;
-      this.stocks = [...this.stocks, ...stocks];
+      this.storeStockService.stocks = [...this.storeStockService.stocks, ...stocks];
       this.page++;
     });
   }
@@ -124,30 +125,41 @@ export class PersonalStockComponent implements OnInit {
       this.sortedBy
     ).subscribe((stocks: Stock[])=> {
       this.isLoading = false;
-      this.stocks = stocks;
+      this.storeStockService.stocks = stocks;
       this.page++;
     });
   }
 
   delete(stock: Stock) {
     const id: number = stock.id
-    this.stockService.delete(stock.ingredient.id).subscribe(()=> {
-      this.stocks = this.stocks.filter(stock=>stock.id!==id);
-      this.notification.blank(stock.ingredient.title + " successfully removed from your stock", "")
+    this.stockService.delete(stock.ingredient.id).subscribe(() => {
+      this.storeStockService.stocks = this.storeStockService.stocks.filter(stock=>stock.id!==id);
+      this.notification.success(stock.ingredient.title + " successfully removed from your stock", "")
       this.ingredient = stock.ingredient;
+      },
+      () => {
+        this.notification.error("Failed to delete " + stock.ingredient.title, "");
     });
   }
 
   change(stock: Stock) {
     this.stockService.update(stock.ingredient.id, stock.amount).subscribe(()=>{
-      this.notification.blank("Stock  " + stock.ingredient.title + " changed", "")
+      this.notification.success("Stock  " + stock.ingredient.title + " changed", "")
+      },
+      () => {
+        this.notification.error("Failed to change stock", "");
     });
   }
 
   addStock(stockAdd: StockAddDto): void {
-    this.stockService.create(stockAdd).subscribe((res: Stock)=> {
-      this.stocks.push(res);
-      this.notification.blank("You successfully added ingredient to your stock", "")
+    this.stockService.create(stockAdd).subscribe((stock: Stock)=> {
+      this.storeStockService.stocks.push(stock);
+      this.storeStockService.ingredients = this.storeStockService.ingredients.filter(
+        ingredient=>ingredient.id!==stockAdd.ingredientId);
+      this.notification.success("You successfully added ingredient to your stock", "")
+      },
+      () => {
+        this.notification.error("Failed to add ingredient to your stock", "");
     });
   }
 
